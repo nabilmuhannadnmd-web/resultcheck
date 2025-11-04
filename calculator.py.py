6b906e1cd2456abc24b3ce0edc6b53324541bb8c
Nabil Muhannad <nabilmuhannadnmd@gmail.com>
2025-11-04 05:15:19 -0800

    Add files via upload

---
+++ calculator.py.py
@@ -0,0 +1,19 @@
+a = int(input("Enter your 1st number: "))
+s = input("Enter yout symbol: ")
+b = int(input("Enter your 2nd number: "))
+
+if(s=='+'):
+    print(a+b)
+
+elif(s=='-'):
+    print(a-b)
+
+elif(s=='*'):
+    print(a*b)
+
+elif(s=='/'):
+    print(a/b)
+
+elif(s=='**'):
+    print(a**b)
+
