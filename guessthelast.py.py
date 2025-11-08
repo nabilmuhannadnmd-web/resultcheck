ef98152c1360c3ca7974f7b367c0aaa9c0032248
Nabil Muhannad <nabilmuhannadnmd@gmail.com>
2025-11-07 21:01:16 -0800

    Add files via upload

---
+++ guessthelast.py.py
@@ -0,0 +1,14 @@
+name = "nabil"
+print(name)
+
+a = input("write last words: ")
+
+print(name.endswith(a))
+
+if(True):
+    print("you are ginius")
+
+#i dont know why dont works this
+elif(False):
+    print("you are idiot")
+
